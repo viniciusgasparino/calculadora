c75a960fa540d4e81f8812ae291600e9c9b7d3cf
viniciusgasparino <viniciusgasparino@yahoo.com.br>
2022-10-17 17:56:12 -0300

    importado component button

--- components/butom/Button.jsx
+++ components/butom/Button.jsx
@@ -1,11 +1,23 @@
 
 
 
-function Button(props){
+
+function Button({text,onClick}){
   return(
-    <button>{props.text}</button>
+    <button onClick={onClick} style={{
+      backgroundColor: "lightblue",
+      border: "none",
+      width: "40px",
+      cursor: "pointer",
+      height: "40px",
+      margin: "10px",
+      color: "blue"
+    }}>
+      {text}
+    </button>
   )
 }
+
 
 
 export default Button
--- components/calculadora/Calculadora.jsx
+++ components/calculadora/Calculadora.jsx
@@ -5,7 +5,24 @@ function Calculadora(){
   return(
     <>
       calculadora
-      <Button text="22"/>
+      <Button text="1" />
+      <Button text="2" />
+      <Button text="3" />
+      <Button text="4" />
+      <Button text="5" />
+      <Button text="6" />
+      <Button text="7" />
+      <Button text="8" />
+      <Button text="9" />
+      <Button text="0" />
+      <Button text="AC"/>
+      <Button text="+" />
+      <Button text="-" />
+      <Button text="*" />
+      <Button text="/" />
+      <Button text="%" />
+      <Button text="." />
+      <Button text="<-"/>
     </>
   )
 }
