35ce39629397cb78a70f2a85dd17901b657af3c3
viniciusgasparino <viniciusgasparino@yahoo.com.br>
2022-10-17 16:55:33 -0300

    Criado component button

---
+++ components/butom/Button.jsx
@@ -0,0 +1,11 @@
+
+
+
+function Button(props){
+  return(
+    <button>{props.text}</button>
+  )
+}
+
+
+export default Button
--- components/calculadora/Calculadora.jsx
+++ components/calculadora/Calculadora.jsx
@@ -1,10 +1,11 @@
-
+import Button from "../butom/Button"
 
 
 function Calculadora(){
   return(
     <>
       calculadora
+      <Button text="22"/>
     </>
   )
 }
