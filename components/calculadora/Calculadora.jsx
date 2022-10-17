145b19f224cd223ced53b2743fded169fd77ad7f
viniciusgasparino <viniciusgasparino@yahoo.com.br>
2022-10-17 20:41:45 -0300

    criado div tela

--- components/calculadora/Calculadora.jsx
+++ components/calculadora/Calculadora.jsx
@@ -4,28 +4,29 @@ import styles from "./Calculadora.module.css"
 
 function Calculadora(){
   return(
-    <>
-    <div className={styles.content}>
-      <Button text="1" corBotao="green"/>
-      <Button text="2" />
-      <Button text="3" />
-      <Button text="4" />
-      <Button text="5" />
-      <Button text="6" />
-      <Button text="7" />
-      <Button text="8" />
-      <Button text="9" />
-      <Button text="0" />
-      <Button text="AC"/>
-      <Button text="+" />
-      <Button text="-" />
-      <Button text="*" />
-      <Button text="/" />
-      <Button text="%" />
-      <Button text="." />
-      <Button text="<-"/>
+    <div className={styles.container}>
+      <div className={styles.tela}><h2></h2></div>
+      <div className={styles.content}>
+        <Button text="1" corBotao="green"/>
+        <Button text="2" />
+        <Button text="3" />
+        <Button text="4" />
+        <Button text="5" />
+        <Button text="6" />
+        <Button text="7" />
+        <Button text="8" />
+        <Button text="9" />
+        <Button text="0" />
+        <Button text="AC"/>
+        <Button text="+" />
+        <Button text="-" />
+        <Button text="*" />
+        <Button text="/" />
+        <Button text="%" />
+        <Button text="." />
+        <Button text="<-"/>
+      </div>
     </div>
-    </>
   )
 }
 
